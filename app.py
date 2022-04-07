61b9e11519cc09fee9b2d9a953878a43abb1f4ab
Marie Valdovinos <marie.valdovinos@amtil.co>
2022-04-07 16:46:57 -0500

    created class constructor

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
-import tkinter as tk # allows us to create classes
+import tkinter as tk # providing classes for defining later
 
+class Snake(tk.Canvas): # creating Canvas widgets to display text, lines, & graphics
+    def __init__(self): # constructing a parent widget
+        super().__init__(width=600, height=620, background="black", highlightthickness=0)
