9581911b59bc7e572ef90de51c551f0441511aaf
Marie Valdovinos <marie.valdovinos@amtil.co>
2022-04-07 18:40:23 -0500

    added root properties

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ import tkinter as tk # providing classes for defining later
 class Snake(tk.Canvas): # creating Canvas widgets to display text, lines, & graphics
     def __init__(self): # constructing a parent widget
         super().__init__(width=600, height=620, background="black", highlightthickness=0)
+
+root=tk.Tk()
+root.title("Snake")
+root.resizable(False, False) #  tells window manager whether this is resizeable
+
+board = Snake()
+
+root.mainloop() #calling the mainloop of tk
