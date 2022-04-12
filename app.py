3e442a877c74be0625a30745c949fc0b110762bb
Marie Valdovinos <marie.valdovinos@amtil.co>
2022-04-12 00:15:44 -0500

    added try-except block to app.py

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-import tkinter as tk # providing classes for defining later
+import tkinter as tk 
+# providing classes for defining later
+from PIL import Image, ImageTk
 
 class Snake(tk.Canvas): # creating Canvas widgets to display text, lines, & graphics
     def __init__(self): # constructing a parent widget
         super().__init__(width=600, height=620, background="black", highlightthickness=0)
 
-root=tk.Tk()
+        self.load_assets()
+
+        def load_assets(self):
+            # define our method to allow the app to load images
+            try:
+                self.snake_body_image = Image.open("./assets/snake.png")
+                self.snake_body = ImageTk.PhotoImage(self.snake_body_image)
+
+                self.food_image = Image.open("./assets/food.png")
+                self.food_image = ImageTk.PhotoImage(self.food_image)
+            except
+                    IOError as error:
+                    print(error)
+                    root.destroy()
+
+root = tk.Tk()
 root.title("Snake")
 root.resizable(False, False) #  tells window manager whether this is resizeable
 
