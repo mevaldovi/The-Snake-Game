24eb0886a3d8ec66395c16a46fc6f912ce31533a
Marie Valdovinos <marie.valdovinos@amtil.co>
2022-05-29 14:42:20 -0500

    added lines 26-28 in app.py

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ class Snake(tk.Canvas): # creating Canvas widgets to display text, lines, & grap
     def __init__(self): # constructing a parent widget
         super().__init__(width=600, height=620, background="black", highlightthickness=0)
 
+        self.snake_positions = [(100, 100), (80, 100), (60, 100)]
+        self.food_position = (200, 100)
+
         self.load_assets()
+        self.create_objects()
 
         def load_assets(self):
             # define our method to allow the app to load images
@@ -17,7 +21,13 @@ class Snake(tk.Canvas): # creating Canvas widgets to display text, lines, & grap
                 self.food_image = ImageTk.PhotoImage(self.food_image)
             except IOError as error:
                     print(error) # js eqivalent of console.log(err)
-                    root.destroy() # close windown and stop application
+                    # root.destroy() # close window and stop application
+
+def create_objects(self):
+    for x_position, y_position in self.snake_positions:
+        self.create_image(x_position, y_position, image=self.snake_body, tag="snake")
+
+
 
 # some-code-here
 root = tk.Tk()
